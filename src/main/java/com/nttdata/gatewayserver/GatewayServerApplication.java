bcf42160ea5b25fcbf00beffb1f7279e218736dd
Jordi Diaz <jordi.diaz04@hotmail.com>
2022-03-10 15:57:53 -0500

    Update code for future checkstyle plugin.

--- src/main/java/com/nttdata/gatewayserver/GatewayServerApplication.java
+++ src/main/java/com/nttdata/gatewayserver/GatewayServerApplication.java
@@ -2,15 +2,15 @@ package com.nttdata.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
+/**
+ * Gateway-Server application, enable eureka client.
+ */
 @EnableEurekaClient
 @SpringBootApplication
 public class GatewayServerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(GatewayServerApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayServerApplication.class, args);
+  }
 }
